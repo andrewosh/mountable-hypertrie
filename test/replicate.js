eb57ab29022f4ba4ae2e6d5a53ce184aa7f0c65b
Andrew Osheroff <andrewosh@gmail.com>
2019-05-08 15:28:55 +0200

    Add replication test

---
+++ test/replicate.js
@@ -0,0 +1,10 @@
+const test = require('tape')
+
+const { create } = require('./helpers/create')
+const { runAll } = require('./helpers/util')
+
+const MountableHypertrie = require('..')
+
+test('simple replication')
+
+
